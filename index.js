45f5d37ac92bf6186c60b030e3afcfe1715f0531
Umri Marsyifa <umrimarsyifa2@gmail.com>
2024-06-28 10:57:49 +0700

    git init

---
+++ index.js
@@ -0,0 +1,16 @@
+document.writeln("Hello world");
+document.writeln("</br>");
+document.writeln(100);
+document.writeln("</br>");
+document.writeln(0b10101);
+document.writeln(true);
+document.writeln(false);
+document.writeln("</br>");
+document.writeln("<textarea cols= '100' rows = '10' >");
+document.writeln("Nama \t: Umri Marsyifa \nKelas \t: Udah lulus");
+document.writeln("</textarea>")
+document.writeln("</br>");
+const fullName = "Umri Marsyifa";
+
+//fullName = "saya";
+document.writeln(fullName);
